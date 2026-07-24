d100ec6a658f48de6ec78b878e886c2f9d47370c
guoshaoyang-pku <161094568+guoshaoyang-pku@users.noreply.github.com>
2026-07-24 15:36:00 +0800

    Deploy static site directly to main branch for GitHub Pages

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://guoshaoyang-pku.github.io",
-  output: "static",
-  build: {
-    format: "directory",
-  },
-});
